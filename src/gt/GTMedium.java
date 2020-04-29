5834bbdf4029924f4991b793563ed6837e165274
Travis Mewborne <tmewborne@hotmail.com>
2020-04-29 18:49:25 -0400

    sad

---
+++ src/gt/GTMedium.java
@@ -0,0 +1,11 @@
+package gt;
+
+import org.aiwolf.sample.player.SampleMedium;
+
+public class GTMedium extends AbstractMedium {
+
+	public GTMedium() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
