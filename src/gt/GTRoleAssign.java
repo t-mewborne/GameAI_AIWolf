20c6e558bf3ad32044391e3b499a670ed3599374
Travis Mewborne <tmewborne@hotmail.com>
2020-04-29 18:04:47 -0400

    yikes

---
+++ src/gt/GTRoleAssign.java
@@ -0,0 +1,17 @@
+package gt;
+
+import org.aiwolf.sample.lib.AbstractRoleAssignPlayer;
+
+public class GTRoleAssign extends AbstractRoleAssignPlayer {
+
+	public GTRoleAssign() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
